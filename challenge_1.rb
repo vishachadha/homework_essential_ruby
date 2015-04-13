a5699cd1675c6ec3fccda87fd5d870586205c853
vishachadha <Visha@dhcp-10-101-96-97.wireless.northwestern.private>
2015-04-13 04:20:40 -0500

    Started working on challenge_1

--- challenge_1.rb
+++ challenge_1.rb
@@ -18,6 +18,10 @@ def pmt(rate, nper, pv)
   # Your code to implement the method goes here.
   # You shouldn't have to write or change code anywhere else.
   # =========================================================
+  monthly_payment = 0.0
+  monthly_payment = rate * pv/(1- (1+rate)**(-nper))
+  
+  return monthly_payment
 end
 
 # Example usage of the method is below. Uncomment to test your pmt method,
